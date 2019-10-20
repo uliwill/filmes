059df6de9d22f36c7d2b3a17e7351a2bea2ec129
Ulisses Willian Reichenbach <uliwill@gmail.com>
2019-10-19 21:29:26 -0300

    Configurações iniciais e rotas

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Erro extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Página não encontrada!</h2>
+            </div>
+        );
+    }
+}
+
+
+export default Erro;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import './home.css';
+
+class Home extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            filmes: []
+        };
+        this.loadFilmes = this.loadFilmes.bind(this);
+    }
+
+    componentDidMount(){
+        this.loadFilmes();
+    }
+
+    loadFilmes(){
+        let url = 'https://sujeitoprogramador.com/r-api/?api=filmes';
+        fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+            this.setState({filmes: json});
+            console.log(json);
+        });
+    }
+    
+    render() {
+        return (
+            <div className="container">
+                <div className="lista-filmes">
+                    {this.state.filmes.map((filme) => {
+                        return(
+                            <article key={filme.id} className="filme">
+                                <strong>{filme.nome}</strong>
+                                <img src={filme.foto} alt="Pôster do filme" />
+                                <Link to="/">Acessar</Link>
+                            </article>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
